38a430762fbcbafd9f188d920d3b3af4b6c5cdd0
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2025-06-26 15:22:04 +0300

    feat: added java project for solutions

---
+++ solutions/java-solutions/src/main/java/com/mikkkkkkka/solutions/_1TwoSum.java
@@ -0,0 +1,11 @@
+package com.mikkkkkkka.solutions;
+
+public class _1TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++)
+            for (int j = i + 1; j < nums.length; j++)
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+        throw new IllegalArgumentException("No solution found");
+    }
+}
---
+++ solutions/java-solutions/src/test/java/com/mikkkkkkka/_1TwoSumTest.java
@@ -0,0 +1,33 @@
+package com.mikkkkkkka;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.mikkkkkkka.solutions._1TwoSum;
+
+public class _1TwoSumTest {
+    @Test
+    public void testCases() {
+        int[] nums1 = { 2, 7, 11, 15 };
+        int target1 = 9;
+        int[] expected1 = { 0, 1 };
+        Assertions.assertArrayEquals(expected1, _1TwoSum.twoSum(nums1, target1));
+
+        int[] nums2 = { 3, 2, 4 };
+        int target2 = 6;
+        int[] expected2 = { 1, 2 };
+        Assertions.assertArrayEquals(expected2, _1TwoSum.twoSum(nums2, target2));
+
+        int[] nums3 = { 3, 3 };
+        int target3 = 6;
+        int[] expected3 = { 0, 1 };
+        Assertions.assertArrayEquals(expected3, _1TwoSum.twoSum(nums3, target3));
+    }
+
+    @Test
+    public void testNoSolution() {
+        int[] nums = { 1, 2, 3 };
+        int target = 6;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> _1TwoSum.twoSum(nums, target));
+    }
+}
