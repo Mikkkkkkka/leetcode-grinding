576af3d30be68beb73d83abc90a551d172f6940c
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2025-06-26 23:34:18 +0300

    feat: created a python3 project for solutions

---
+++ solutions/python3_solutions/solutions/p1_two_sum.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(0, len(nums)-1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    raise Exception('No solution found')
---
+++ solutions/python3_solutions/tests/test_1_two_sum.py
@@ -0,0 +1,28 @@
+import pytest
+import solutions.p1_two_sum as p1
+
+
+class Test1TwoSum:
+    def test_case_1(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        expected = [0, 1]
+        assert p1.twoSum(nums, target) == expected
+        
+    def test_case_2(self):
+        nums = [3, 2, 4]
+        target = 6 
+        expected = [1, 2]
+        assert p1.twoSum(nums, target) == expected
+        
+    def test_case_3(self):
+        nums = [3, 3]
+        target = 6
+        expected = [1, 2]
+        assert p1.twoSum(nums, target) == expected
+        
+    def test_throws_on_no_solution(self):
+        nums = [1, 2, 3]
+        target = 6
+        with pytest.raises(Exception):
+            p1.twoSum(nums, target)
