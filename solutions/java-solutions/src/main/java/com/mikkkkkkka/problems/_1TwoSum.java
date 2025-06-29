2c28ed48a685686f2fd009a1d7790f7629ce6979
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2025-06-29 19:48:55 +0300

    feat: added 'sort' and 'hash' solutions to problem 1 to the java project

--- solutions/java-solutions/src/main/java/com/mikkkkkkka/problems/_1TwoSum.java
+++ solutions/java-solutions/src/main/java/com/mikkkkkkka/problems/_1TwoSum.java
@@ -1,11 +1,47 @@
 package com.mikkkkkkka.problems;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class _1TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumBrute(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++)
             for (int j = i + 1; j < nums.length; j++)
                 if (nums[i] + nums[j] == target)
                     return new int[] { i, j };
+        throw new IllegalArgumentException("No solution found");
+    }
+
+    private record NumAndIndex(int value, int index) {
+        public static int Comparator(NumAndIndex a, NumAndIndex b) {
+            return a.value - b.value;
+        }
+    }
+
+    public static int[] twoSumSort(int[] nums, int target) {
+        var numsWithIndex = new NumAndIndex[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            numsWithIndex[i] = new NumAndIndex(nums[i], i);
+        Arrays.sort(numsWithIndex, NumAndIndex::Comparator);
+        for (int i = 0; i < nums.length; i++) {
+            int desired = target - numsWithIndex[i].value;
+            int found = Arrays.binarySearch(numsWithIndex, i + 1, nums.length, new NumAndIndex(desired, 0),
+                    NumAndIndex::Comparator);
+            if (found > 0)
+                return new int[] { numsWithIndex[i].index, numsWithIndex[found].index };
+        }
+        throw new IllegalArgumentException("No solution found");
+    }
+
+    public static int[] twoSumHash(int[] nums, int target) {
+        Map<Integer, Integer> processedNums = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int desired = target - nums[i];
+            if (processedNums.containsKey(desired))
+                return new int[] { processedNums.get(desired), i };
+            processedNums.put(nums[i], i);
+        }
         throw new IllegalArgumentException("No solution found");
     }
 }
--- solutions/java-solutions/src/test/java/com/mikkkkkkka/_1TwoSumTest.java
+++ solutions/java-solutions/src/test/java/com/mikkkkkkka/_1TwoSumTest.java
@@ -7,27 +7,41 @@ import com.mikkkkkkka.problems._1TwoSum;
 
 public class _1TwoSumTest {
     @Test
-    public void testCases() {
+    public void testCase1() {
         int[] nums1 = { 2, 7, 11, 15 };
         int target1 = 9;
         int[] expected1 = { 0, 1 };
-        Assertions.assertArrayEquals(expected1, _1TwoSum.twoSum(nums1, target1));
+        Assertions.assertArrayEquals(expected1, _1TwoSum.twoSumBrute(nums1, target1));
+        Assertions.assertArrayEquals(expected1, _1TwoSum.twoSumSort(nums1, target1));
+        Assertions.assertArrayEquals(expected1, _1TwoSum.twoSumHash(nums1, target1));
+    }
 
+    @Test
+    public void testCase2() {
         int[] nums2 = { 3, 2, 4 };
         int target2 = 6;
         int[] expected2 = { 1, 2 };
-        Assertions.assertArrayEquals(expected2, _1TwoSum.twoSum(nums2, target2));
+        Assertions.assertArrayEquals(expected2, _1TwoSum.twoSumBrute(nums2, target2));
+        Assertions.assertArrayEquals(expected2, _1TwoSum.twoSumSort(nums2, target2));
+        Assertions.assertArrayEquals(expected2, _1TwoSum.twoSumHash(nums2, target2));
+    }
 
+    @Test
+    public void testCase3() {
         int[] nums3 = { 3, 3 };
         int target3 = 6;
         int[] expected3 = { 0, 1 };
-        Assertions.assertArrayEquals(expected3, _1TwoSum.twoSum(nums3, target3));
+        Assertions.assertArrayEquals(expected3, _1TwoSum.twoSumBrute(nums3, target3));
+        Assertions.assertArrayEquals(expected3, _1TwoSum.twoSumSort(nums3, target3));
+        Assertions.assertArrayEquals(expected3, _1TwoSum.twoSumHash(nums3, target3));
     }
 
     @Test
     public void testNoSolution() {
         int[] nums = { 1, 2, 3 };
         int target = 6;
-        Assertions.assertThrows(IllegalArgumentException.class, () -> _1TwoSum.twoSum(nums, target));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> _1TwoSum.twoSumBrute(nums, target));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> _1TwoSum.twoSumSort(nums, target));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> _1TwoSum.twoSumHash(nums, target));
     }
 }
